5b765218462806e9c2e168d2c39b098a971ec205
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2023-03-14 15:22:54 -0400

    Test

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -125,64 +125,64 @@ noaa_wl <- function(id, type, begin_date, end_date){
     return(wl)
   }
 
-fiman_wl <- function(id, begin_date, end_date){
-  print("fiman_wl entered")
-  station_keys <- fiman_gauge_key %>% 
-    filter(site_id == id) %>% 
-    filter(Sensor == "Water Elevation")
+fiman_wl <- function(site_id, begin_date, end_date){
+  # print("fiman_wl entered")
+  # station_keys <- fiman_gauge_key %>% 
+  #   filter(site_id == id) %>% 
+  #   filter(Sensor == "Water Elevation")
   
-  request <- httr::GET(url = Sys.getenv("FIMAN_URL"),
-                       query = list(
-                         "site_id" = station_keys$site_id,
-                         "data_start" = paste0(format(begin_date, "%Y-%m-%d %H:%M:%S")),
-                         "date_end" = paste0(format(end_date, "%Y-%m-%d %H:%M:%S")),
-                         "format_datetime"="%Y-%m-%d %H:%M:%S",
-                         "tz" = "UTC",
-                         "show_raw" = T,
-                         "show_quality" = T,
-                         "sensor_id" =  station_keys$sensor_id
+  # request <- httr::GET(url = Sys.getenv("FIMAN_URL"),
+  #                      query = list(
+  #                        "site_id" = station_keys$site_id,
+  #                        "data_start" = paste0(format(begin_date, "%Y-%m-%d %H:%M:%S")),
+  #                        "date_end" = paste0(format(end_date, "%Y-%m-%d %H:%M:%S")),
+  #                        "format_datetime"="%Y-%m-%d %H:%M:%S",
+  #                        "tz" = "UTC",
+  #                        "show_raw" = T,
+  #                        "show_quality" = T,
+  #                        "sensor_id" =  station_keys$sensor_id
                          
-                       ))
+  #                      ))
   
-  content <- request$content %>% 
-    xml2::read_xml() %>% 
-    xml2::as_list() %>% 
-    as_tibble()
+  # content <- request$content %>% 
+  #   xml2::read_xml() %>% 
+  #   xml2::as_list() %>% 
+  #   as_tibble()
   
-  print("FIMAN response received")
-  parsed_content <- content$onerain$response %>% 
-    as_tibble() %>% 
-    unnest_wider("general") %>% 
-    unnest(cols = names(.)) %>% 
-    unnest(cols = names(.)) %>% 
-    mutate(data_time = lubridate::ymd_hms(data_time),
-           data_value = as.numeric(data_value))
+  # print("FIMAN response received")
+  # parsed_content <- content$onerain$response %>% 
+  #   as_tibble() %>% 
+  #   unnest_wider("general") %>% 
+  #   unnest(cols = names(.)) %>% 
+  #   unnest(cols = names(.)) %>% 
+  #   mutate(data_time = lubridate::ymd_hms(data_time),
+  #          data_value = as.numeric(data_value))
   
-  wl <- parsed_content %>%
-    transmute(
-      id = id,
-      date = data_time,
-      level = data_value,
-      entity = "FIMAN",
-      notes = "observation"
+  # wl <- parsed_content %>%
+  #   transmute(
+  #     id = id,
+  #     date = data_time,
+  #     level = data_value,
+  #     entity = "FIMAN",
+  #     notes = "observation"
     )
 
   print(wl, n=1000)
 
-  # wl <- local_water_levels %>% 
-  #     filter(id == site_id, date >= begin_date, date <= end_date) %>%
-  #     select(id, date, value, api_name) %>%
-  #     arrange(date) %>%
-  #     transmute(
-  #       id = id,
-  #       date = date,
-  #       level = value,
-  #       entity = api_name,
-  #       notes = "observation"
-  #     ) %>%
-  #     as_tibble()
+  wl <- local_water_levels %>% 
+      filter(id == site_id, date >= begin_date, date <= end_date) %>%
+      select(id, date, value, api_name) %>%
+      arrange(date) %>%
+      transmute(
+        id = id,
+        date = date,
+        level = value,
+        entity = api_name,
+        notes = "observation"
+      ) %>%
+      as_tibble()
 
-  # print(wl, n=40)
+  print(wl, n=200)
 
   return(wl)
 }
@@ -196,7 +196,7 @@ get_local_wl <- function(wl_id, wl_src, type = c("obs"), begin_date, end_date) {
                           type = type,
                           begin_date = begin_date,
                           end_date = end_date),
-         "FIMAN" = fiman_wl(id = wl_id,
+         "FIMAN" = fiman_wl(site_id = wl_id,
                             begin_date = begin_date,
                             end_date = end_date)
   )
